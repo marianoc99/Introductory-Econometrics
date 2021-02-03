e9f5240bd5bdff1a6be70e8a17a27c9e9b6c0b56
marianoc99 <78475819+marianoc99@users.noreply.github.com>
2021-02-03 11:37:53 +0100

    Update C1.R

--- Chapter 1/C1.R
+++ Chapter 1/C1.R
@@ -1,21 +1,20 @@
 COMPUTER EXERCISES (PAGE 17/18)
 
 (C1)(i)
-load("C:/Users/carmo/OneDrive/Escritorio/S/R data sets for 5e/wage1.RData")
+load("~/wage1.RData")
 education_level <- data$educ
 summary(education_level)
 
 sum(education_level==0)
 sum(education_level==18)
 
-The minimum year of education is 0 and the maximum is 18 years.
-2 people have 0 years of education and 19 people have 18 years of education.
+#The minimum year of education is 0 and the maximum is 18 years. 2 people have 0 years of education and 19 people have 18 years of education.
 
 (ii)
 hourly_wage<-data$wage
 mean(hourly_wage)
 
-The hourly average wage is 5.896103. It seems to be low.
+# The hourly average wage is 5.896103. It seems to be low.
 
 (iii)
 CPI_1976 <- 56.9
@@ -24,7 +23,7 @@ CPI_2010<-218.056
 iv)
 hourly_wage_2010 <-hourly_wage*CPI_2010/CPI_1976
 mean(hourly_wage_2010)
-The average wage in 2010 is $22.60, which seems to be high
+# The average wage in 2010 is $22.60, which seems to be high
 
 (v)
 female<-sum(data$female)
@@ -32,4 +31,4 @@ female
 male <-526- female
 male
 
-There are 252 women and 274 men
+# There are 252 women and 274 men
